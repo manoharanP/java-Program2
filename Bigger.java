7dcc311b30363645b56d30890d187788e5160277
manoharanP <30316786+manoharanP@users.noreply.github.com>
2017-07-25 15:50:14 +0530

    Bigger.java
    To find Big Number

---
+++ Bigger.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Bigger
+{
+public static void main()
+{ 
+int Number1,Number2,Number3;
+Scanner num=new Scanner(System.in);
+num=Number1.nextInt();
+num=Number2.nextInt();
+num=Number3.nextInt():
+if(Number1>Number2 && Number1>Number3)
+System.out.println("Number1 is Bigger");
+else if(Number2>Number3)
+System.out.println("Number2 Is Bigger");
+else
+System.out.println("Number3 Is Bigger");
+}
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# java-Program2
-To find the greatest number Among Three Numbers
